6085790d517f93058eb43f9f4e4e86c2c725c953
Souvik Jana <souvikjana2013@gmail.com>
2024-06-27 21:02:23 +0530

    feat: connect with DB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome to API of chat app");
+});
+
+const connectionString = process.env.DB_CONNECTION_STRING;
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to DB"))
+  .catch((error) => console.log("Couldn't connect to DB", error));
+
 const PORT = process.env.PORT || "8000";
-app.listen("8000", () =>
+app.listen(PORT, () =>
   console.log(`App is running at http://localhost:${PORT}`)
 );
